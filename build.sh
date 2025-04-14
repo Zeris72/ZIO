cf2ecb52a03c9efc1e2ba11007776e2260da3365
Maciej Zygmunt <uterek58@gmail.com>
2025-04-14 17:38:11 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p out
-javac -d out -cp .:junit-4.13.2.jar src/Hello.java test/HelloTest.java
-java -cp out:.:junit-4.13.2.jar:hamcrest-core-1.3.jar org.junit.runner.JUnitCore HelloTest
+javac -d out -cp lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar src/Hello.java test/HelloTest.java
+java -cp out:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore HelloTest
