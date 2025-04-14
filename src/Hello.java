f3416bc43a1ba3dd81e41e071b4f59eba079e6ff
Maciej Zygmunt <uterek58@gmail.com>
2025-04-14 17:21:46 +0200

    Create Hello.java

---
+++ src/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Jenkins!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
